743e6e56830167d6dcbe14b574d97b7ab82b00e0
Shevy Mittal <shevy.mittal@gslab.com>
2019-08-20 17:00:10 +0530

    committing knime://knime.mountpoint/Files/git_path.txt

--- Airbnb_master.py
+++ Airbnb_master.py
@@ -63,6 +63,7 @@ with mlflow.start_run():
 	
 	mlflow.log_param("alpha", alpha)
 	mlflow.log_param("l1_ratio", l1_ratio)
+	mlflow.log_param("Model","ElasticNet")
 	mlflow.log_metric("rmse", rmse)
 	mlflow.log_metric("r2", r2)
 	mlflow.log_metric("mae", mae)
