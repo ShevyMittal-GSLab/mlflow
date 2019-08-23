ca048c38663a519a0ef7e64fcc55b58d668cbff6
ShevyMittal-GSLab <shevy.mittal@gslab.com>
2019-08-23 09:06:08 +0000

    changes in XGBoost Regression.py

--- MLExperiments/XGBoost Regression.py
+++ MLExperiments/XGBoost Regression.py
@@ -19,23 +19,25 @@ def eval_metrics(actual, pred):
 	mae = mean_absolute_error(actual, pred)
 	r2 = r2_score(actual, pred)
 	return rmse, mae, r2 
-df = spark.sql('select * from knime_datasets.queens').toPandas() 
-target = "Review_Scores_Rating5"
+df = spark.sql('select * from knime_datasets.queens_bronx').toPandas() 
+target = "Price"
+df = df[df.Price.notnull()]
+df['Price'] = pd.to_numeric(df['Price'],errors='coerce')
 df = df[df.Review_Scores_Rating5.notnull()]
-df['Review_Scores_Rating5'] = df.Review_Scores_Rating5.astype(float)
-df = df[df.Number_of_Records.notnull()]
-df['Number_of_Records'] = df.Number_of_Records.astype(float)
+df['Review_Scores_Rating5'] = pd.to_numeric(df['Review_Scores_Rating5'],errors='coerce')
+df = df[df.Beds.notnull()]
+df['Beds'] = pd.to_numeric(df['Beds'],errors='coerce')
 df = df[df.Number_Of_Reviews.notnull()]
-df['Number_Of_Reviews'] = df.Number_Of_Reviews.astype(float)
+df['Number_Of_Reviews'] = pd.to_numeric(df['Number_Of_Reviews'],errors='coerce')
 df = df[df.Review_Scores_Rating12.notnull()]
-df['Review_Scores_Rating12'] = df.Review_Scores_Rating12.astype(float)
+df['Review_Scores_Rating12'] = pd.to_numeric(df['Review_Scores_Rating12'],errors='coerce')
 train, test = train_test_split(df)
-train_x = train[['Number_of_Records', 'Number_Of_Reviews', 'Review_Scores_Rating12']]
-test_x = test[['Number_of_Records', 'Number_Of_Reviews', 'Review_Scores_Rating12']]
-train_y = train[["Review_Scores_Rating5"]]
-test_y = test[["Review_Scores_Rating5"]]
-height = [5010, 32, 2, 39, 4, 23, 7, 225, 83, 254, 311, 1933, 2333, 5412, 4227]
-bars = ['100', '20', '30', '40', '45', '50', '55', '60', '65', '70', '75', '80', '85', '90', '95']
+train_x = train[['Review_Scores_Rating5', 'Beds', 'Number_Of_Reviews', 'Review_Scores_Rating12']]
+test_x = test[['Review_Scores_Rating5', 'Beds', 'Number_Of_Reviews', 'Review_Scores_Rating12']]
+train_y = train[["Price"]]
+test_y = test[["Price"]]
+height = [1, 2, 71, 1, 1, 1, 2, 16, 2, 4, 9, 41, 1, 1, 2, 17, 2, 11, 51, 1, 1, 43, 2, 1, 7, 23, 1, 1, 23, 2, 1, 3, 22, 2, 1, 2, 9, 1, 7, 71, 3, 1, 1, 1, 4, 25, 1, 8, 1, 4, 9, 1, 23, 1, 1, 2, 6, 5, 1, 1, 7, 7, 1, 10, 1, 1, 5, 30, 3, 1, 4, 1, 1, 1, 6, 1, 9, 1, 3, 1, 2, 1, 1, 1, 21, 1, 2, 1, 5, 1, 3, 2, 1, 2, 1, 2, 11, 1, 1, 2, 1, 11, 3, 1, 1, 2, 1, 1, 1, 2, 1, 1, 4, 1, 4, 1, 1, 5, 1, 2, 1, 7, 1, 1, 1, 3, 13, 1, 1, 1, 5, 16, 2, 2, 1, 1, 19, 1, 1, 1, 6, 11, 20, 3, 1, 1, 29, 1, 1, 3, 2, 17, 35, 4, 3, 2, 30, 2, 4, 6, 42]
+bars = ['1,111', '1,500', '100', '101', '102', '103', '104', '105', '107', '108', '109', '110', '112', '113', '114', '115', '118', '119', '120', '123', '124', '125', '126', '127', '129', '130', '131', '132', '135', '137', '138', '139', '140', '142', '143', '144', '145', '148', '149', '150', '155', '156', '157', '158', '159', '160', '162', '165', '168', '169', '170', '172', '175', '176', '178', '179', '180', '185', '186', '188', '189', '190', '192', '195', '196', '198', '199', '200', '205', '209', '210', '212', '215', '219', '220', '222', '225', '229', '230', '232', '235', '238', '240', '244', '250', '255', '260', '270', '275', '277', '279', '280', '288', '290', '295', '299', '300', '305', '315', '325', '330', '350', '375', '380', '395', '4,000', '400', '420', '45', '450', '460', '475', '49', '5,000', '50', '500', '52', '55', '569', '59', '590', '60', '600', '625', '63', '64', '65', '66', '675', '68', '69', '70', '700', '71', '72', '725', '75', '750', '76', '77', '78', '79', '80', '81', '83', '84', '85', '850', '86', '87', '88', '89', '90', '92', '93', '94', '95', '950', '97', '98', '99']
 y_pos = np.arange(len(bars))
 plt.bar(y_pos, height)
 plt.xticks(y_pos, bars)
@@ -50,9 +52,9 @@ colsample_bytree = 0.3
 max_depth = 3
 objective = 'reg:linear'
 n_estimators = 5
-subsample = 0.2
-gamma = 10
-reg_lambda = 10
+subsample = 10
+gamma = 5
+reg_lambda = 5
 
 mlflow.set_tracking_uri("http://10.43.13.1:5000")
 experiment_name = "XGBoost Regression"
