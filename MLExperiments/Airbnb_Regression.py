31356754d9b2ca218faa8376cc64dd20bae89255
ShevyMittal-GSLab <shevy.mittal@gslab.com>
2019-10-10 12:00:16 +0530

    changes in Airbnb_Regression.py

--- MLExperiments/Airbnb_Regression.py
+++ MLExperiments/Airbnb_Regression.py
@@ -19,61 +19,49 @@ def eval_metrics(actual, pred):
 	mae = mean_absolute_error(actual, pred)
 	r2 = r2_score(actual, pred)
 	return rmse, mae, r2 
-<<<<<<< HEAD
-df = spark.sql('select * from knime_datasets.queens_bronx').toPandas() 
+df = spark.sql('select * from knime_datasets.brooklyn_manhattan').toPandas() 
 target = "Price"
 df['Price'] = pd.to_numeric(df['Price'],errors='coerce')
 df['Review_Scores_Rating5'] = pd.to_numeric(df['Review_Scores_Rating5'],errors='coerce')
-df['Number_Of_Reviews'] = pd.to_numeric(df['Number_Of_Reviews'],errors='coerce')
-=======
-df = spark.sql('select * from knime_datasets.tbl11').toPandas() 
-target = "Price"
-df['Price'] = pd.to_numeric(df['Price'],errors='coerce')
-df['Review_Scores_Rating5'] = pd.to_numeric(df['Review_Scores_Rating5'],errors='coerce')
->>>>>>> branch 'master' of https://github.com/ShevyMittal-GSLab/mlflow.git
 df['Review_Scores_Rating12'] = pd.to_numeric(df['Review_Scores_Rating12'],errors='coerce')
 df = df.dropna(how='any',axis=0)
 train, test = train_test_split(df)
-<<<<<<< HEAD
-train_x = train[['Review_Scores_Rating5', 'Number_Of_Reviews', 'Review_Scores_Rating12']]
-test_x = test[['Review_Scores_Rating5', 'Number_Of_Reviews', 'Review_Scores_Rating12']]
-train_y = train[["Price"]]
-test_y = test[["Price"]]
-height = [1, 2, 71, 1, 1, 1, 2, 16, 2, 4, 9, 41, 1, 1, 2, 17, 2, 11, 51, 1, 1, 43, 2, 1, 7, 23, 1, 1, 23, 2, 1, 3, 22, 2, 1, 2, 9, 1, 7, 71, 3, 1, 1, 1, 4, 25, 1, 8, 1, 4, 9, 1, 23, 1, 1, 2, 6, 5, 1, 1, 7, 7, 1, 10, 1, 1, 5, 30, 3, 1, 4, 1, 1, 1, 6, 1, 9, 1, 3, 1, 2, 1, 1, 1, 21, 1, 2, 1, 5, 1, 3, 2, 1, 2, 1, 2, 11, 1, 1, 2, 1, 11, 3, 1, 1, 2, 1, 1, 1, 2, 1, 1, 4, 1, 4, 1, 1, 5, 1, 2, 1, 7, 1, 1, 1, 3, 13, 1, 1, 1, 5, 16, 2, 2, 1, 1, 19, 1, 1, 1, 6, 11, 20, 3, 1, 1, 29, 1, 1, 3, 2, 17, 35, 4, 3, 2, 30, 2, 4, 6, 42]
-bars = ['1,111', '1,500', '100', '101', '102', '103', '104', '105', '107', '108', '109', '110', '112', '113', '114', '115', '118', '119', '120', '123', '124', '125', '126', '127', '129', '130', '131', '132', '135', '137', '138', '139', '140', '142', '143', '144', '145', '148', '149', '150', '155', '156', '157', '158', '159', '160', '162', '165', '168', '169', '170', '172', '175', '176', '178', '179', '180', '185', '186', '188', '189', '190', '192', '195', '196', '198', '199', '200', '205', '209', '210', '212', '215', '219', '220', '222', '225', '229', '230', '232', '235', '238', '240', '244', '250', '255', '260', '270', '275', '277', '279', '280', '288', '290', '295', '299', '300', '305', '315', '325', '330', '350', '375', '380', '395', '4,000', '400', '420', '45', '450', '460', '475', '49', '5,000', '50', '500', '52', '55', '569', '59', '590', '60', '600', '625', '63', '64', '65', '66', '675', '68', '69', '70', '700', '71', '72', '725', '75', '750', '76', '77', '78', '79', '80', '81', '83', '84', '85', '850', '86', '87', '88', '89', '90', '92', '93', '94', '95', '950', '97', '98', '99']
-=======
 train_x = train[['Review_Scores_Rating5', 'Review_Scores_Rating12']]
 test_x = test[['Review_Scores_Rating5', 'Review_Scores_Rating12']]
 train_y = train[["Price"]]
 test_y = test[["Price"]]
-height = [15, 2, 4, 2, 9, 1, 1, 9, 1, 2, 11, 1, 18, 1, 2, 9, 1, 1, 8, 2, 12, 1, 5, 3, 1, 23, 3, 1, 1, 2, 7, 5, 1, 3, 12, 1, 5, 1, 1, 1, 1, 1, 1, 2, 15, 2, 2, 1, 2, 1, 6, 2, 2, 3, 3, 1, 1, 1, 2, 1, 1, 1, 9, 1, 1, 2, 7, 3, 6, 1, 1, 1, 4, 2, 5, 1, 8]
-bars = ['100', '101', '105', '109', '110', '111', '112', '115', '118', '119', '120', '122', '125', '128', '129', '130', '131', '133', '135', '139', '140', '143', '145', '148', '149', '150', '155', '156', '157', '159', '160', '165', '169', '170', '175', '179', '180', '185', '186', '188', '190', '195', '198', '199', '200', '205', '210', '215', '220', '222', '225', '235', '240', '250', '275', '285', '289', '299', '300', '325', '385', '480', '75', '76', '78', '79', '80', '83', '85', '86', '88', '89', '90', '92', '95', '98', '99']
->>>>>>> branch 'master' of https://github.com/ShevyMittal-GSLab/mlflow.git
-y_pos = np.arange(len(bars))
-plt.bar(y_pos, height)
-plt.xticks(y_pos, bars)
-plt.ylabel("count")
-plt.xlabel(target)
+
+N = 60
+g1 = (0.6 + 0.6 * np.random.rand(N), np.random.rand(N))
+g2 = (0.4+0.3 * np.random.rand(N), 0.5*np.random.rand(N))
+g3 = (0.3*np.random.rand(N),0.3*np.random.rand(N))
+
+data = (g1, g2, g3)
+colors = ("red", "green", "blue")
+groups = ("coffee", "tea", "water")
+
+# Create plot
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+for data, color, group in zip(data, colors, groups):
+	x,y = data
+	ax.scatter(x, y, alpha=0.8, c=color, edgecolors='none', s=30, label=group)
+
+plt.title('Matplot scatter plot')
+plt.legend(loc=2)
 plt.show()
 plt.savefig("target_count_plot.png")
 
-<<<<<<< HEAD
-alpha = 0.1
-l1_ratio = 0.1
-random_state = 42
-max_iter = None
-mlflow_run_name = 'ElasticNet' + '_0'
-=======
 alpha = 0
 learning_rate = 0.3
-colsample_bytree = 0.5
+colsample_bytree = 1
 max_depth = 6
 objective = 'reg:linear'
 n_estimators = 1
 subsample = 1
 gamma = 0
 reg_lambda = 1
->>>>>>> branch 'master' of https://github.com/ShevyMittal-GSLab/mlflow.git
 
 mlflow.set_tracking_uri("http://10.43.13.1:5000")
 experiment_name = "Airbnb_Regression"
@@ -91,16 +79,12 @@ with mlflow.start_run():
 	mlflow.log_param("learning_rate", learning_rate)
 	mlflow.log_param("max_depth", max_depth)
 	mlflow.log_param("alpha", alpha)
-<<<<<<< HEAD
-	mlflow.log_param("l1_ratio", l1_ratio)
-=======
 	mlflow.log_param("n_estimators", n_estimators)
 	mlflow.log_param("gamma", gamma)
 	mlflow.log_param("lambda", reg_lambda)
 	mlflow.log_param("subsample", subsample)
-	mlflow.log_param("Model","XGBoost")
 	
->>>>>>> branch 'master' of https://github.com/ShevyMittal-GSLab/mlflow.git
+	
 	mlflow.log_metric("rmse", rmse)
 	mlflow.log_metric("r2", r2)
 	mlflow.log_metric("mae", mae)
